6041b871beae58b225b21a6b12c2c0f67df91e5f
Roman Marinichev <rmarinicev@gmail.com>
2022-11-12 23:17:05 +0300

    Amount Value to string

--- src/Models/Ets/Amount.go
+++ src/Models/Ets/Amount.go
@@ -1,11 +1,11 @@
 package Ets
 
 type Amount struct {
-	Value    float64 `json:"value"`
-	Currency string  `json:"currency"` // 3 символа
+	Value    string `json:"value"`
+	Currency string `json:"currency"` // 3 символа
 }
 
-func NewAmount(value float64, currency string) *Amount {
+func NewAmount(value string, currency string) *Amount {
 	return &Amount{
 		Value:    value,
 		Currency: currency,
